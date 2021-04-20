0348f314037ec71143768623af78caef4bcbcf1f
Muhammad Salik <75317422+saliksian@users.noreply.github.com>
2021-04-20 12:13:19 +0500

    final commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// you can take number 1 & number 2 from user
+let number1 = 765;
+let number2 = 65;
+
+console.log(Math.max(number1,number2)) // Math.max method returns maximum number between numbers or array
+
